f2869a3e62fb24e7779a99ac120ff54d3424ed66
elisamaistri <10531069@polimi.it>
2020-03-26 23:56:11 +0100

    prima versione di classi Board, Cell e Worker

--- src/main/java/it/polimi/ingsw/PSP027/LorenzoBello.java
+++
@@ -1,7 +0,0 @@
-package it.polimi.ingsw.PSP027;
-
-public class LorenzoBello {
-    int ciaociao = 0;
-    int hei = 27;
-    int cisiamo= 123;
-}
---
+++ src/main/java/it/polimi/ingsw/PSP027/Model/Worker.java
@@ -0,0 +1,56 @@
+package it.polimi.ingsw.PSP027.Model;
+
+/**
+ * @author Elisa Maistri
+ */
+
+public class Worker {
+
+    private int index;
+    private Cell position;
+    private Player owner;
+
+    /**
+     * Constructor: creates a new worker, storing who is its owner and its index, setting its position to null
+     * @param owner is the player who owns the worker
+     * @param index index that tells which one of the two workers of said player this worker is
+     */
+
+    public Worker(Player owner, int index)
+    {
+        this.owner = owner;
+        this.index = index;
+        position = null;
+    }
+
+    /**
+     * Method used to get the position of the worker on the board
+     * @return the cell that the worker is occupying
+     */
+
+    public Cell getWorkerPosition ()
+    {
+        return position;
+    }
+
+    /**
+     * Method used to get the index of the worker, which can be 1 or 2
+     * @return the index
+     */
+
+    public int getWorkerIndex ()
+    {
+        return index;
+    }
+
+    /**
+     * Method that gets the worker's owner
+     * @return the Player, owner of the worker
+     */
+
+    public Player getWorkerOwner ()
+    {
+        return owner;
+    }
+
+}
