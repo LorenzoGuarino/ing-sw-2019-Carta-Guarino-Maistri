8d393f94fd0050cecd3c252fc9881364cd8a2b93
icsdaniel <10573704@polimi.it>
2020-05-01 20:09:06 +0200

    CommitTest

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,8 @@
 open module it.polimi.ingsw.PSP027
 
-{ requires javafx.controls; requires javafx.fxml;
+{
+    requires javafx.controls;
+    requires javafx.fxml;
     requires javafx.graphics;
     requires java.xml;
     requires java.compiler;
