61104dd6a4b6e98300068f10f588eb0b9329e0d4
icsdaniel <10573704@polimi.it>
2020-05-12 15:55:32 +0200

    Start BoardController

---
+++ src/main/java/it/polimi/ingsw/PSP027/View/Controllers/BoardPageController.java
@@ -0,0 +1,44 @@
+package it.polimi.ingsw.PSP027.View.Controllers;
+
+import it.polimi.ingsw.PSP027.View.GUI;
+import javafx.fxml.FXML;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+
+public class BoardPageController {
+    private GUI gui;
+
+    @FXML
+    public ImageView A1;
+    public ImageView A2;
+    public ImageView A3;
+    public ImageView A4;
+    public ImageView A5;
+    public ImageView B1;
+    public ImageView B2;
+    public ImageView B3;
+    public ImageView B4;
+    public ImageView B5;
+    public ImageView C1;
+    public ImageView C2;
+    public ImageView C3;
+    public ImageView C4;
+    public ImageView C5;
+    public ImageView D1;
+    public ImageView D2;
+    public ImageView D3;
+    public ImageView D4;
+    public ImageView D5;
+    public ImageView E1;
+    public ImageView E2;
+    public ImageView E3;
+    public ImageView E4;
+    public ImageView E5;
+
+    public Image level1 = new Image("images/");
+    public Image level2 = new Image("images/");
+    public Image level3 = new Image("images/");
+
+
+}
